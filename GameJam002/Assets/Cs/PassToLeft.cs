532211530fe54496fc65c1a41a82f21ea5409f42
Alex Magee <alex.magee16701@gmail.com>
2020-02-02 14:59:55 -0500

    Passing objects works now
    buuuut the objects can be combined if both are passed one after the other

--- GameJam002/Assets/Cs/PassToLeft.cs
+++ GameJam002/Assets/Cs/PassToLeft.cs
@@ -3,18 +3,18 @@
 public class PassToLeft : MonoBehaviour
 {
 
-    public GameObject ThisWire;
+    //public GameObject ThisWire;
     public GameObject WirePrefab;
     //public GameObject LeftSpawnLocation;
 
     void OnTriggerEnter(Collider coll)
     {
         Debug.Log("trigger enter ");
-        if (coll.gameObject.tag == "PassObj")
+        if (coll.gameObject.name == "RightCollider")
         {
             GameObject SpawnLoc = GameObject.Find("LeftSpawnLoc");
             Debug.Log("coll with PassObj");
-            Destroy(ThisWire);
+            Destroy(this.gameObject);
             Instantiate(WirePrefab, new Vector3(SpawnLoc.transform.position.x, SpawnLoc.transform.position.y, SpawnLoc.transform.position.z), Quaternion.identity);
         }
     }
--- GameJam002/Assets/Cs/PassToRight.cs
+++ GameJam002/Assets/Cs/PassToRight.cs
@@ -3,18 +3,18 @@
 public class PassToRight : MonoBehaviour
 {
 
-    public GameObject ThisWire;
+    //public GameObject ThisWire;
     public GameObject WirePrefab;
     //public GameObject RightSpawnLocation;
 
     void OnTriggerEnter(Collider coll)
     {
         Debug.Log("trigger enter ");
-        if (coll.gameObject.tag == "PassObj")
+        if (coll.gameObject.name == "LeftCollider")
         {
             GameObject SpawnLoc = GameObject.Find("RightSpawnLoc");
             Debug.Log("coll with PassObj");
-            Destroy(ThisWire);
+            Destroy(this.gameObject);
             Instantiate(WirePrefab, new Vector3(SpawnLoc.transform.position.x, SpawnLoc.transform.position.y, SpawnLoc.transform.position.z), Quaternion.identity);
         }
     }
