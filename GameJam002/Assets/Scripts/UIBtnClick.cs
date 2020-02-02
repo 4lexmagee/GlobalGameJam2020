8621c0ae9f5ca706876db1b9c9199726cbdab82e
Alex Magee <alex.magee16701@gmail.com>
2020-02-02 17:12:00 -0500

    Built game

---
+++ GameJam002/Assets/Scripts/UIBtnClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIBtnClick : MonoBehaviour
+{
+    public string ToLevel;
+    void Start()
+    {
+        Button b = gameObject.GetComponent<Button>();
+        b.onClick.AddListener(delegate () {
+            Debug.Log("uievent, loading scene: " + ToLevel);
+            SceneManager.LoadScene(ToLevel.Trim());
+        });
+    }
+}
