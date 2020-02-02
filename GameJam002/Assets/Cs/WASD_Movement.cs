c153e4d1d10b6c6c76ea25fc167f2dbb695e5605
saf122 <60560498+saf122@users.noreply.github.com>
2020-02-02 00:00:56 -0500

    Both Player Movement

---
+++ GameJam002/Assets/Cs/WASD_Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_Movement : MonoBehaviour
+{
+    public float speed = 20f;
+    //public float speed = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    //Vector3 pos = transform.position;
+
+    //    //if (Input.GetKey("w"))
+    //    //{
+    //    //    Debug.Log("afsdfasdfd");
+    //    //    pos.z += speed;// * Time.deltaTime;
+    //    //}
+    //    //if (Input.GetKey("s"))
+    //    //{
+    //    //    pos.z -= speed * Time.deltaTime;
+    //    //}
+    //    //if (Input.GetKey("d"))
+    //    //{
+    //    //    pos.x += speed * Time.deltaTime;
+    //    //}
+    //    //if (Input.GetKey("a"))
+    //    //{
+    //    //    pos.x -= speed * Time.deltaTime;
+    //    //}
+
+
+    //}
+
+
+    private void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.A))
+            //rb.AddForce(Vector3.left);
+            rb.velocity = -transform.right * speed;
+        if (Input.GetKey(KeyCode.D))
+            rb.velocity = transform.right * speed;
+        if (Input.GetKey(KeyCode.W))
+            //rb.AddForce(Vector3.up);
+            rb.velocity = new Vector3(0, 0, 1) * speed;
+        if (Input.GetKey(KeyCode.S))
+            //rb.AddForce(Vector3.down);
+            rb.velocity = new Vector3(0, 0, -1) * speed;
+
+    }
+
+
+}
---
+++ GameJam002/Assets/Cs/arrowKeys_Movement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowKeys_Movement : MonoBehaviour
+{
+    public float speed = 20f;
+    //public float speed = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    //Vector3 pos = transform.position;
+
+    //    //if (Input.GetKey("w"))
+    //    //{
+    //    //    Debug.Log("afsdfasdfd");
+    //    //    pos.z += speed;// * Time.deltaTime;
+    //    //}
+    //    //if (Input.GetKey("s"))
+    //    //{
+    //    //    pos.z -= speed * Time.deltaTime;
+    //    //}
+    //    //if (Input.GetKey("d"))
+    //    //{
+    //    //    pos.x += speed * Time.deltaTime;
+    //    //}
+    //    //if (Input.GetKey("a"))
+    //    //{
+    //    //    pos.x -= speed * Time.deltaTime;
+    //    //}
+
+
+    //}
+
+
+    private void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey("left"))
+            //rb.AddForce(Vector3.left);
+            rb.velocity= - transform.right * speed;
+        if (Input.GetKey("right"))
+            rb.velocity = transform.right * speed;
+        if (Input.GetKey("up"))
+            //rb.AddForce(Vector3.up);
+            rb.velocity = new Vector3(0,0,1) * speed;
+        if (Input.GetKey("down"))
+            //rb.AddForce(Vector3.down);
+            rb.velocity = new Vector3(0, 0, -1) * speed;
+
+    }
+
+
+}
